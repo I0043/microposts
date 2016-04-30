ac70c70eed0d8a5d1d86533f5c997e35b916f0d0
i0043 <yfe27571@nifty.com>
2016-04-30 00:05:48 +0000

    add posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
+  end
   
   before_action :set_user, only: [:edit, :update]
   
